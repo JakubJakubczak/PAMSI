28803bee8bcf7b5c1e2a6e9bfb8eff015b8418da
Jakub Jakubczak <263486@student.pwr.edu.pl>
2023-04-16 15:47:39 +0200

    Program1Dzialajacy

--- kolejkapriorytetowa.cpp
+++ kolejkapriorytetowa.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 // klasa przechowujaca podzielony tekst, klucz i wskaznik do nastepnego elementu jesli taki istnieje
 class elementkol{
@@ -201,11 +203,20 @@ elementkol kolejkaprio::removeMin()
 
 }
 
+void mixarray(elementkol* tab[], int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        int j= rand() % count;
+        std::swap(tab[i],tab[j]);
+    }
+   // return tab;
+}   
+
 int main()
 {
     std::string wiadomosc;
-    std::string wiadomoscwyslana;
-    std::string wiadomoscodebrana;
+    std::string wiadomoscposortowaniu;
     int dlugoscwiadomosci;
     int dlugoscpakietumin;
     int j=0;
@@ -214,6 +225,9 @@ int main()
     elementkol* pakiet3 = new elementkol(3);
     elementkol* pakiet4 = new elementkol(4);
     elementkol* pakiet5 = new elementkol(5);
+    kolejkaprio kolejka;
+    std::string pakiet[5];
+    srand( time( NULL ) ); // czas do zmiennej losowej
 
     getline(std::cin,wiadomosc);
 
@@ -225,11 +239,42 @@ int main()
     pakiet3->pakiet=wiadomosc.substr(dlugoscpakietumin*2,dlugoscpakietumin);
     pakiet4->pakiet=wiadomosc.substr(dlugoscpakietumin*3,dlugoscpakietumin);
     pakiet5->pakiet=wiadomosc.substr(dlugoscpakietumin*4);
+
+    elementkol* pakiety[]={pakiet1,pakiet2,pakiet3,pakiet4,pakiet5};
     
+    mixarray(pakiety,5);
+    std::cout<<"Wiadomosc podzielona i polosowana: ";
+     for (int i = 0; i < 5; i++)
+    {
+        std::cout<<pakiety[i]->pakiet<<" ";
+    }
 
+    // dodawanie elementow do kolejki priorytetowej
+    for ( int i = 0; i < 5; i++)
+    {
+        kolejka.insert(pakiety[i]->klucz, pakiety[i]->pakiet);
+    }
     
+    // usuwanie elementow do kolejki priorytetowej i zapisywanie ich do stringow
+    for (int i = 0; i < 5; i++)
+    {
+        pakiet[i]=kolejka.removeMin().pakiet;
+    }
 
+    // laczenie posortowanych pakietow w wiadomosc
 
+    for (int i = 0; i < 5; i++)
+    {
+        wiadomoscposortowaniu=wiadomoscposortowaniu+pakiet[i];
+    }
+    
+    // wypisanie wiadomosci po sortowaniu
+
+    std::cout<<std::endl;
+    std::cout<<"Wiadomosc po sortowaniu "<<wiadomoscposortowaniu<<std::endl;
+    
+    
+    
 
     delete pakiet1;
     delete pakiet2;
